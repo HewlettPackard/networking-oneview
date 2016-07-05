c6db533d79b5d241a1b18f5e3ed0d834762620c5
SauloAislan <sauloa@lsd.ufcg.edu.br>
2016-07-05 15:58:51 +0000

    Fix db and add install script

---
+++ install-driver.sh
@@ -0,0 +1,5 @@
+NEUTRON_DIR=${1:-/opt/stack/neutron}
+
+cp -r networking_oneview/db $NEUTRON_DIR/neutron/
+cp -r networking_oneview/ml2 $NEUTRON_DIR/neutron/plugins/
+cp -r networking_oneview/tests/unit/ml2 $NEUTRON_DIR/neutron/tests/unit/plugins
